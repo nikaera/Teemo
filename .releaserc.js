b8474d358143a6ba2aad6e45caa6bb8e3e5fcfc7
nikaera <gimnikaerast@gmail.com>
2021-06-12 10:32:18 +0900

    :ambulance: fix: add the packages needed to run semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -15,22 +15,22 @@ module.exports = {
       },
     ],
     [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "build.zip",
+            label: "Teemo - Chrome Extension",
+          },
+        ],
+      },
+    ],
+    [
       "@semantic-release/git",
       {
         message:
           // eslint-disable-next-line no-template-curly-in-string
           ":bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}",
-      },
-    ],
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "teemo-chrome.zip",
-            label: "Teemo 💕 - Chrome Extension",
-          },
-        ],
       },
     ],
   ],
